068ea6ada872c6812c979b401dae3b85ca4b2ce7
GomerkkuJ <131454885+GomerkkuJ@users.noreply.github.com>
2023-05-14 17:36:31 +0500

    Update settings.py

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+#s-+j(7%g^#txvunf*ugpy+=7zjr9^3)#y(9sl%bj=clkp$9-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['1234987127371.pythonanywhere.com']
 
 
 # Application definition
